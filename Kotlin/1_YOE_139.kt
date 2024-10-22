0720c277433f16dd867c94d2697110d2d86f4b70
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-10-22 11:59:51 +0530

    Update and rename 1_YOE_Kotlin_41.kt to 1_YOE_139.kt

--- Kotlin/1_YOE_139.kt
+++ Kotlin/1_YOE_139.kt
@@ -200,6 +200,217 @@
 -> Marker interface is a special type of interface that doesn't have property and method declaration 
 -> IT use to make class special for certain requirements 
 -> interface Premium
--> class Account: Premium  (You can create object and save to Premium interface so account will change to premium)
+-> class Account: Premium  (You can create an object and save to the Premium interface so the account will change to premium)
 
+========================================================================================================================
+ 1. What is Kotlin?
+
+2. How is Kotlin different from Java?
+
+3. What are the basic data types in Kotlin?
+-> Byte, Short, Int, Long, Float, Double, Char, Boolean, String
+
+4. What are null safety features in Kotlin?
+
+5. What is a data class in Kotlin?
+
+6. How to declare a data class in Kotlin?
+
+7. What is a companion object in Kotlin?
+
+8. What is the difference between val and var?
+
+9. What is the use of lateinit?
+
+10. How to define a function in Kotlin?
+
+11. What is an extension function?
+
+12. What is an example of an extension function?
+
+13. What are coroutines in Kotlin?
+
+14. How do you create a coroutine in Kotlin?
+
+15. What is suspend in Kotlin?
+
+16. How to handle nulls in Kotlin using ?. operator?
+
+17. What is the Elvis operator in Kotlin?
+
+18. How to declare an array in Kotlin?
+
+19. What is let in Kotlin?
+
+20. How to iterate over a list in Kotlin?
+
+21. What is a sealed class?
+
+22. When to use a sealed class?
+
+23. What is apply in Kotlin?
+
+24. How do you define a singleton in Kotlin?
+3 Ways 1) Traditional 2) object 3) companion object
+
+25. What are higher-order functions in Kotlin?
+
+26. What is inline in Kotlin?
+
+27. Explain the lazy keyword in Kotlin.
+
+28. What is a backing field in Kotlin?
+
+29. What are the primary constructors in Kotlin?
+
+30. How to create a secondary constructor in Kotlin?
+
+31. What is the == operator in Kotlin?
+-> == for structural equality
+
+32. How does the when expression work in Kotlin?
+
+33. What is the use of by keyword in Kotlin?
+-> It use to property deligation and implement delegate pattern
+
+34. What are destructuring declarations?
+
+35. Example of destructuring declaration?
+
+36. What is a lambda expression in Kotlin?
+
+37. What is with in Kotlin?
+
+38. How does forEach differ from a for loop?
+
+39. What is a map in Kotlin?
+-> collection of key-value pair
+
+40. How to declare a map in Kotlin?
+
+41. What is filter in Kotlin?
+
+42. How to check if a variable is of a particular type? (is keyword)
+
+43. What is the difference between List and Array?
+
+44. What is the infix keyword in Kotlin?
+
+45. How to convert a String to an Int in Kotlin? ("1".toInt())
+
+46. What is enum in Kotlin?
+-> It represent fix set of constants
+
+47. How do you create an enum class?
+enum class ClassName { C1, C2 ... }
+
+48. What is a tailrec function?
+
+49. What is a range in Kotlin?
+
+50. What is a mutable list?
+
+51. What is an init block in Kotlin?
+
+52. How can you make a class open for inheritance in Kotlin? (by open keyword)
+
+53. What is the purpose of the super keyword in Kotlin?
+
+54. What is typealias in Kotlin?
+It gives alternatives name to existing data type
+
+55. Example of typealias?
+-> typealias onClick = (Int,Int) -> Unit
+
+56. What is a sealed interface?
+-> It restrict which type can implement interface
+ 
+57. How to declare a default value for a function parameter?
+
+58. What are vararg parameters?
+
+59. Example of a function with vararg parameter:
+fun show(vararg nums: Int)
+
+60. What is the difference between runBlocking and launch in coroutines?
+
+61. What are nested and inner classes in Kotlin?
+-> Inner class need object of outer class, nested dont
+ 
+62. What is crossinline in Kotlin? (REDO)
+
+63. What is the use of noinline in Kotlin?
+
+64. How to create an abstract class in Kotlin?
+
+65. What is the purpose of by lazy?
+
+66. How to declare a constant in Kotlin?
+
+67. What is the use of sealed in Kotlin?
+
+68. What is inline property?
+
+69. What is a unit in Kotlin?
+
+70. What is the with function used for?
+
+71. How to handle exceptions in Kotlin?
+
+72. What is the use of the finally block?
+
+73. What is the is keyword in Kotlin?
+
+74. How to convert a MutableList to an Immutable List? (.toList())
+
+75. What are @JvmStatic, @JvmOverloads, and @JvmField annotations?
+
+76. What is the difference between suspend and blocking functions?
+
+77. What is the default visibility modifier in Kotlin?(public)
+
+78. How to convert a Kotlin String to a CharArray? ("aa".toCharArray())
+
+79. What is the use of super in Kotlin constructors?
+
+80. How to check if a list is empty?
+
+81. What is the difference between let and run?
+
+82. How to convert a String to List of characters?
+
+83. What are infix functions?
+
+84. How to use super with multiple interfaces having the same method?
+
+85. What is a recursive function in Kotlin?
+
+86. How to declare an immutable map in Kotlin?
+
+87. What is the purpose of checkNotNull in Kotlin?
+
+88. What is the use of as keyword in Kotlin?
+
+89. How to concatenate strings in Kotlin?
+ 
+90. What is the difference between throw and throws?
+
+91. What is takeIf in Kotlin?
+
+92. How to create a thread in Kotlin?
+
+93. What are the types of collections in Kotlin?
+
+94. What is destructuring in Kotlin?
+
+95. What is the difference between == and === in Kotlin?
+-> == : structure equality , === referanctial equality
+
+96. How to convert a list to a set in Kotlin?
+-> listOf(1, 2, 2, 3).toSet()
+
+97. What is flow in Kotlin?
+-> Stream of data asynchronusly
+
+98. What is coroutineScope in Kotlin?
 
