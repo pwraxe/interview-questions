09b7cd73186bf926987091a3e62f738ae7c55f61
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-10-22 12:09:43 +0530

    Update and rename 2_Year_Exp_84Q.kt to 2_YOE_88.kt

--- Kotlin/2_YOE_88.kt
+++ Kotlin/2_YOE_88.kt
@@ -17,7 +17,7 @@
 ==OOP
 
 6. What is an object in Kotlin?
--> Instance of that class which contains actual data and behavious of that class
+-> Instance of that class which contains actual data and behaviour of that class
 
 7. What are the types of constructors in Kotlin?
 -> Primary and Secondary
@@ -25,98 +25,98 @@
 8. Explain the data class in Kotlin.
 
 9. What is the sealed class in Kotlin?
--> Restrict Inheritance Heirarchy, Compiler knows all heirarchi at compile time, sealed keyword, Should be in same file
+-> Restrict Inheritance Hierarchy, Compiler knows all hierarchy at compile time, sealed keyword, Should be in same file
 
-10. What is the object keyword used for in Kotlin?
--> Object is Special keyword in kotlin 
--> It acts like a static in java
+10. What is the object keyword used in Kotlin?
+-> Object is a Special keyword in kotlin 
+-> It acts like a static in Java
 -> We can directly access properties and methods directly object name
 -> It is singleton by default
--> It use to create singleton object, declare anonymous class and object expression
+-> It is used to create singleton objects, declare anonymous classes and object expression
  
 ==FUNCTIONAL
 
 11. What is a higher-order function in Kotlin?
--> A function which accept and return lambda function
+-> A function that accepts and returns the lambda function
 
 12. What is an extension function in Kotlin?
 
 13. What is the lambda expression in Kotlin?
--> lambda function which can pass as arg or assign to variable
+-> lambda function which can pass as arg or assign to a variable
 
 14. What is the let function in Kotlin?
--> let is scope function
--> Ideally it run on nullable variable if it is not null
--> It cast null variable to not null for data
--> it provides it as current scope context
+-> Let is scope function
+-> Ideally it runs on nullable variable if it is not null
+-> It cast the null variable to not null for data
+-> It provides it as the current scope context
 
 15. What are inline functions in Kotlin?
--> copy body of inline function at caller place
--> it reduce calling same function again and again
--> It improves performance and reduce memory occupation
+-> Copy the body of the inline function at the caller's place
+-> It reduces calling the same function again and again
+-> It improves performance and reduces memory occupation
 
 ==NULL SAFTY
 16. How does Kotlin handle null values?
 -> Kotlin does not support nullability by default
 -> We have to make variables nullable explicitly
--> If we have nullable variable then kotlin provides 4 diff ways to handle null values
+-> If we have a nullable variable then kotlin provides 4 different ways to handle null values
 -> 1) Safe Call 2) Elvis Operator 3) Assertion 4) Let Block
 
 17. What is the !! operator in Kotlin?
--> !! is Assertion operator (Risky Operator)
+-> !! is an Assertion operator (Risky Operator)
 
 18. What is the Elvis operator (?:) in Kotlin?
--> It is short syntax to handle null variable
--> if nullable variable is not null then it return as it is else return default value
--> It is more clean and concise way to handle nullable variable 
+-> It is a short syntax to handle the null variable
+-> If the nullable variable is not null then it returns as it else returns the default value
+-> It is a more clean and concise way to handle nullable variable 
 
 19. How can you handle null values using safe calls in Kotlin?
 -> ?.
 
-20. What is the purpose of let in null safety?
--> Let is scope function which gives us `it` as current scope context.
--> It execute block of code only if nullable variable is not null
--> it cast nullable to non-nullable to safly work on object
--> It allows us to create weak refrance of an object
+20. What is the purpose of letting in null safety?
+-> Let is scope function which gives us `it` as the current scope context.
+-> It executes a block of code only if the nullable variable is not null
+-> It cast nullable to non-nullable to safely work on an object
+-> It allows us to create weak references of an object
 
 
 21. What are coroutines in Kotlin?
--> Concurrancy Design Pattern, 
+-> Concurrency Design Pattern, 
 -> Writes Asynchronous code, w/o blocking main thread
 -> Lauch Async,
 -> suspend function
 
 
-22. What is suspend in Kotlin?
--> can control (Start,Pause, Resume, Stop)
+22. What is suspended in Kotlin?
+-> can control (Start, Pause, Resume, Stop)
 -> execute independently
--> call from scope or other suspend function
+-> call from the scope or other suspend function
 
 
-23. What is launch in Kotlin?
--> Launch is Extension function of CoroutineScope
--> Use to create new corutine without blocking main thread
--> it is fire and forget concept
--> We dont get result return back using launch
+23. What is the launch of Kotlin?
+-> Launch is an Extension function of CoroutineScope
+-> Use to create a new coroutine without blocking the main thread
+-> It is fire and forgets the concept
+-> We don't get results to return using the launch
 
 
 24. What is async in Kotlin?
--> Async is Extension function of CoroutineScope
--> Use to create new corutine without blocking main thread
--> It use when we want result back from coroutine
--> It returns Defered type of object, use await to extract data from it
+-> Async is the Extension function of CoroutineScope
+-> Use to create a new coroutine without blocking the main thread
+-> It is used when we want results back from a coroutine
+-> It returns the Deferred type of object, use await to extract data from it
 
 
 25. What is withContext in Kotlin?
--> It is a suspend function which use to switch context of coroutine from one dispatcher to other
+-> It is a suspend function used to switch the context of the coroutine from one dispatcher to other
 -> It can called from either coroutine scope or other suspend function
--> IT uses dispacher to switch the context
+-> IT uses a dispatcher to switch the context
 
 ==KOTLIN COLLECTIONS
 
 26. What is the difference between List and MutableList in Kotlin?
--> Both contains homo and heterogenious element becoz both accept vararg
--> list is immutable hence we cannot add, remove or update element from it, It is read only.
+-> Both contain homo and heterogeneous element because both accept vararg
+-> list is immutable hence we cannot add, remove or update elements from it, It is read-only.
 -> mutableLits if mutable hence we can perform CRUD operation on it
 
 
@@ -129,7 +129,7 @@
 
 
 29. What is the difference between map and flatMap in Kotlin?
--> Map transform each element into another object
+-> Map transforms each element into another object
 -> Flatmap convert elements into a single result 
 
 fun main() {
@@ -140,8 +140,8 @@ fun main() {
 }
 
 
-30. What is the purpose of filter in Kotlin?
--> Filter is use to extract elements from list based on given condition
+30. What is the purpose of the filter in Kotlin?
+-> Filter is used to extract elements from the list based on a given condition
 
 
 ==ANDROID SPECIFIC QUESTION
@@ -150,16 +150,16 @@ fun main() {
 
 
 32. What is the difference between lateinit and lazy in Kotlin?
--> lateinit: late initializatin, mutable, work only non-null variable,  
+-> lateinit: late initialization, mutable, work only non-null variable,  
 -> lazy: delegate property, immutable, work on nullable also
 
 
-33. What is the purpose of companion object in Kotlin?
+33. What is the purpose of a companion object in Kotlin?
 -> Creating Static like fields
 -> Makeing class singleton
 
 34. What is the purpose of @Parcelize in Kotlin?
--> @Parcelize is Android Annotation which makes data class Parcelable by generating boilerplate code
+-> @Parcelize is an Android Annotation that makes data class Parcelable by generating boilerplate code
 
 
 35. What is the purpose of @JvmStatic in Kotlin?
@@ -176,21 +176,21 @@ fun main() {
 
 38. What is the reified keyword in Kotlin?
 -> To retain data info at runtime
--> It uses with inline keywork (inline function)
+-> It uses with inline keyword (inline function)
 -> generic param erased info at compile time, to retain we use reified
 -> fun <reified T> todo(t:T) {}
 
 
 39. What are type aliases in Kotlin?
--> creating alternative name for existing data type
+-> Creating an alternative name for the existing data type
 
 
 40. What are destructuring declarations in Kotlin?
--> Seperating data into variable
+-> Separating data into variable
 
 
 41. What is a range in Kotlin?
--> sequence of values from start to end and its define by .. operator
+-> sequence of values from start to end and it is define by .. operator
 
 42. What is the difference between == and === in Kotlin?
 ==: Structural Equality, ===: Refrancial Equality
@@ -199,14 +199,14 @@ fun main() {
 -> condition statement in kotlin similar to switch in java
 
 44. Explain by keyword in Kotlin.
--> It use to deligation or delate the property
--> Implementation of interface to another object using by
--> Transfer ownership to anather object
+-> It use to delegation or delete the property
+-> Implementation of the interface to another object using by
+-> Transfer ownership to another object
 
 
 43. How do you declare a singleton in Kotlin?
--> 3 Ways to Declared singleton
--> 1)Traditinal Way (Privatizing constructor) 2)object class 3) companion object
+-> 3 Ways to Declared Singleton
+-> 1)Traditional Way (Privatizing constructor) 2)object class 3) companion object
  
 
 44. What is the use of apply in Kotlin?
@@ -217,12 +217,12 @@ fun main() {
 -> try-catch-finally
 
 46. What is an infix function in Kotlin?
--> such function removes dot and parenthesis at caller place of object
+-> such function removes dot and parenthesis at caller's place of the object
 
 47. What is the use of is keyword in Kotlin?
--> It use to check data-type of variable or object
--> It checks whether variable or object is instance of certain type or not
--> It helps to smartcast object
+-> It uses to check the data type of a variable or object
+-> It checks whether a variable or object is an instance of a certain type or not
+-> It helps to smart cast object
 
 
 48. How do you create an array in Kotlin?
@@ -232,7 +232,7 @@ fun main() {
 49. What is a lazy property in Kotlin?
 
 
-50. How does Kotlin handle type casting?
+50. How does Kotlin handle typecasting?
 -> Handle by 
 -> as for Explicite cast
 -> is for Smart cast
@@ -240,17 +240,17 @@ fun main() {
 
 
 51. What is to in Kotlin?
--> It is use to create pair of 2 values
--> It is use to create map entry
+-> It is used to create a pair of 2 values
+-> It is used to create map entry
 
 
 52. How do you handle null pointers in Kotlin?
 4 ways, Safe call, Assertion, Elvis, Let
 
 
-53. What is backing field in Kotlin?
+53. What is the backing field in Kotlin?
 -> getter setter in kotlin 
--> It use when want to customized getter and setter in kotlin
+-> It is used when want to customise getter and setter in kotlin
  
 
 54. Explain lateinit in Kotlin.
@@ -265,14 +265,14 @@ fun main() {
 -> for((k,v) in map) || map.forEach {..}
 
 59. Explain Kotlin Flow.
--> it is stram of data flow, that continuasly emot the values asynchronously
+-> it is stream of data flow, that continuously emit the values asynchronously
 -> It has producer and consumer
--> Producer: continusly produce value and add to stram
--> Consumer: It use to consume data from stream 
--> ColdFlow: It emit value only when it collected (each collector has own instance of flow)
--> HotFlow: It continue emitting value even not collect (multiple collector share same emittion)
--> ShareFlow: Allow multiple collector to share same emission (dont have initial value)
--> StateFlow: It holds single updated state (it has initial value), collector has latest value
+-> Producer: continuously produce value and add to stram
+-> Consumer: It used to consume data from the stream 
+-> ColdFlow: It emits value only when it is collected (each collector has its own instance of flow)
+-> HotFlow: It continues emitting value even if not collected (multiple collector share same emission)
+-> ShareFlow: Allow multiple collectors to share the same emission (don't have initial value)
+-> StateFlow: It holds a single updated state (it has an initial value), the collector has the latest value
 
 
 60. How to create a thread in Kotlin?
@@ -292,7 +292,7 @@ fun main() {
 65. Explain suspend function with an example.
 
 66. What is the difference between join and await in Kotlin coroutines?
--> join: block thread until operation is complete | use on thread    | not returns
+-> join: block thread until the operation is complete | use on thread    | not returns
 -> await: suspend coroutine, wait in other thread | use on coroutine | return Deferred
 
 
@@ -300,10 +300,10 @@ fun main() {
 Coroutine
 
 68. What is a Job in Kotlin coroutines?
--> it is cacellable task with lifecycle, 
--> it control suspend function
+-> It is a cancellable task with a lifecycle, 
+-> it controls the suspend function
 
-69. Explain the use of super keyword in Kotlin.
+69. Explain the use of the super keyword in Kotlin.
 
 70. What is type projection in Kotlin?
 
@@ -316,12 +316,12 @@ Coroutine
 -> toTypedArray()
 
 74. Explain the concept of delegation in Kotlin.
--> It allows one object deligate responsibility to anather objec
--> it implemented using `by` keyword
+-> It allows one object to delegate responsibility to another object
+-> it is implemented using the `by` keyword
 
 
 75. What is const in Kotlin?
--> compile time constant, used with val
+-> compile-time constant, used with val
 
 
 76. What is with function in Kotlin?
@@ -329,7 +329,7 @@ Coroutine
 
 77. What is the purpose of runBlocking in Kotlin?
 -> use to start coroutine 
--> it block main thread until its completion
+-> it blocks main thread until its completion
 
 
 78. Explain the use of Dispatchers in Kotlin coroutines.
@@ -357,8 +357,30 @@ Coroutine
 -> When lambda function calls from inline function kotlin auto makes that inline 
 -> If we don't want then we can specify lambda args as noinline
 
+
 84. What is an inline class in kotlin 
 -> inline class in kotlin is a special type of class used to wrap data
--> This class also called a value class
+-> This class is also called a value class
 -> The main purpose of this class is to use less memory and improve the performance
 -> Inline class have only a single param in the primary constructor
+
+
+85. What is the difference between open and final in Kotlin?
+-> Every class is final by default
+-> Open keyword allows us to extend the class to its subclass
+
+
+86. What is the difference between Any and Unit in Kotlin?
+-> Any is the root class of the kotlin hierarchy similar to the Java Object class
+-> Unit is function default return type if not specified same as void in Java
+
+
+87. What is a primary constructor in Kotlin?
+-> Primary constructors are used to initialised Object
+-> It declares at the class header with an optional constructor keyword
+-> Class properties are auto-initialized at the time of object creation
+-> It also acts as getter setter
+
+
+88. Explain the use of it in Kotlin.
+-> It is a single parameter optional name of the lambda expression
