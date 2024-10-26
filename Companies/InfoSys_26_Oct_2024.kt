dc2627f4a581ca96df9293a62a201432da9e9e66
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-10-26 11:32:25 +0530

    Create InfoSys_26_Oct_2024.kt

---
+++ Companies/InfoSys_26_Oct_2024.kt
@@ -0,0 +1,29 @@
+1. what is oops
+2. explain oops pillers(AEIP)
+3. abstract class vs interface
+4. lateinit vs lazy
+5. How will you check lateinit initialied
+6. const vs val
+7. why use let
+8. explain scope function in kotlin
+9. Tell ActivityLauchMode
+10. Explain coroutine
+11. Explain MVVM
+12. Reposiotry pattern
+13. Explain LiveData 
+14. Which Design pattern you used
+15. Explain companion object
+16. What is solid priciple (Question Passed(skip))
+
+---Jetpack compose
+17. What is remember 
+18. How Compose state survive config changes (rememberSavable)
+19. Explain LauchEffect
+20. What is composition
+21. What is recomposition
+
+
+Problem Statement
+
+1. swap two variable,
+2. fizzbuzz
