69eb0f4d9b708769202cdd937cdf67a2997b0fe0
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-11-23 19:13:16 +0530

    Create InfoSys_23_Nov_2024.kt

---
+++ Companies/InfoSys_23_Nov_2024.kt
@@ -0,0 +1,30 @@
+
+
+- What is Android SDK and its components
+- Tell me Jetpack components
+- What is a singleton class
+- What is an activity
+- Actiivty vs fragment
+- What challenges you faced in your past projects how did you solve it
+- Challenges you face in uploading apk on the play-store
+- How to improve performance, security, and memory management
+- What is service 
+- In the stock app which background component you will use to show updated data on ui
+- Have you worked on SQLite
+- What is WorkManager
+- Background things Thread, Service, WorkManager
+- What is Thread 
+- User Open the App, Click on the Home Button, and Reopen App, which activity lifecycle methods will  get called
+- Last project, challenges, duration
+- MVVM vs MVP
+- What is ViewModel
+- What is LiveData
+- Thread vs async
+- When to use thread?
+- what are Design Patterns, which design pattern was used in the last project
+- What is a retrofit, and how implemented in app
+- How you design an app responsive in different screen sizes
+
+
+Suggestion: Focus on Performance, Optimization, Memory Management, Security
+
