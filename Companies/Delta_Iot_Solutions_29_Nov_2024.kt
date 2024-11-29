752c32dfe19a96cde82788e6184654af9300a213
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2024-11-29 10:35:14 +0530

    Create Delta_Iot_Solutions_29_Nov_2024.kt

---
+++ Companies/Delta_Iot_Solutions_29_Nov_2024.kt
@@ -0,0 +1,34 @@
+1. What is OOPS, Explain four pillers of it
+2. Whats is string
+3. String Literal?
+4. abstract vs interface
+5. does interface has default methods?
+6. Activty vs Fragment
+7. How many way to launch coroutine
+8. I have 4 API, How do I call them parallel
+9. Whats is actvity
+10. What is broadcast receiver
+11. Whats is Flows,  where uses flow in projects
+12. What is channelFlow
+13. async vs launch
+14. suspend vs withcontext
+15. What are four Dispatchers
+16. Explain each Dispatchers
+17. stringbuffer vs stringbuilder
+18. try-catch, 
+19. final finally and finalized
+20. map vs filter
+21. list vs map vs set
+22. arrayList
+23. lateinit vs lazy
+24. What is Android manifest
+25. Have you worked on camera, wifi, bluetooth?
+26. Which MVVM Architecture, OR Clean Architecture
+27. Explain MVVM
+
+
+
+Problem: Reverse String
+
+
+
