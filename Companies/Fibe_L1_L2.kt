a9b8ff2ddc01a8397fc3d08c2ed9bb95cf847ce5
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2025-01-17 17:48:29 +0530

    Create Fibe_L1_L2.kt

---
+++ Companies/Fibe_L1_L2.kt
@@ -0,0 +1,82 @@
+Interview Round 1 || 13 Jan 2025  ||  FIBE L1
+
+1. What is Oops
+2. What are the four pillars of oops
+3. What are App Components
+4. What are Android Components
+5. What is Diff between Foreground, background and bound service
+6. What is the diff between JobScheduler, Work Manager and AlarmService
+7. What are Activity launch modes
+8. Which method calls when the SingleTop mode launch activity
+9. What is the Activity Lifecycle
+10. internal Working of recycler view
+11. internal working of ViewModel
+12. Explain MVVM
+13. how many ways to Communicate with 2 fragments
+14. what is lateinit
+15. what is the difference between lateinit and lazy
+16. what are scope functions
+17. Why kotlin prefer over Java
+18. What is Sealed class?
+19. Give me a use/example of a sealed class excluding the example of using a Network API Call
+20. Where did you use the Extension Function in your code give an example
+21. What is a Broadcast receiver
+22. What is ContentProvider
+------------------------------------------------------------
+Interview Round 2  ||  17 Jan 2025  ||  FIBE L2
+
+1. What is the reason for frequent changes
+2. Tell me something about yourself
+3. What is the difference between XML base design and Compose based design
+4. Have you worked on the Google ML Kit
+5. Which compose application you worked, Was they on Play Store? could you show me? and Explain the use of them (the Interviewer expects How it is implemented?. what library was used init)
+6. CODE: Find the Majority of elements, input: [3, 3, 4, 2, 4, 4, 2, 4, 4], output: 4, Explain code
+
+
+//I have researched about the company from a website and interviews from LinkedIn
+
+
+
+----------------------------------------------------------------
+
+DSA Questions
+✅ https://leetcode.com/problems/two-sum/description/
+✅ https://www.geeksforgeeks.org/find-subarray-with-given-sum/
+✅ https://www.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+✅ https://leetcode.com/problems/rotate-array/description/
+✅ https://leetcode.com/problems/merge-intervals/description/
+✅ https://leetcode.com/problems/valid-anagram/description/
+✅ https://leetcode.com/problems/string-to-integer-atoi/
+✅ https://leetcode.com/problems/group-anagrams/description/
+✅ https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+✅ https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+✅ https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+✅ https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+✅ https://leetcode.com/problems/path-sum/description/
+✅ https://leetcode.com/problems/number-of-islands/description/
+✅ Graph BFS, DFS
+✅ https://leetcode.com/problems/longest-common-subsequence/description/
+✅ https://leetcode.com/problems/fibonacci-number/description/
+✅ https://leetcode.com/problems/single-number/description/
+✅ https://www.geeksforgeeks.org/problems/set-bits0143/1
+
+✅ https://leetcode.com/problems/missing-number/description/
+✅ https://leetcode.com/problems/merge-sorted-array/description/
+✅ https://leetcode.com/problems/maximum-subarray/
+✅ https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+✅ https://leetcode.com/problems/valid-palindrome/
+✅ https://leetcode.com/problems/first-unique-character-in-a-string/description/
+✅ https://leetcode.com/problems/longest-common-prefix/description/
+
+✅ https://leetcode.com/problems/reverse-words-in-a-string/description/
+✅ https://leetcode.com/problems/linked-list-cycle/description/
+✅ https://leetcode.com/problems/reverse-linked-list/description/
+✅ https://leetcode.com/problems/middle-of-the-linked-list/description/
+✅ https://leetcode.com/problems/merge-two-sorted-lists/
+✅ https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+
+Common Design Patterns in Android
+-> MVVM, Repository, Builder, Observer, Factory, Strategy, Adapter, Decorator, State
+  
