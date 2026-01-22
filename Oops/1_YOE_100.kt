dd62e71321450b2d032d9449b5d2c754fa73e997
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2026-01-22 15:49:09 +0530

    Update 1_YOE_100.kt

--- Oops/1_YOE_100.kt
+++ Oops/1_YOE_100.kt
@@ -271,64 +271,122 @@
 50. What is a sealed class in Kotlin?
   - A sealed class is a collection of subclasses that restricts the hierarchy of inheritance
 
+
 51. How do you define a sealed class in Kotlin?
   - by using the sealed keyword
+
 
 52. What is the advantage of using a sealed class?
   - Restricted Inheritance: It allows you to define which classes can extend
   - Compile-time Safety: The compiler knows all possible subclasses at compile time
   - Exhaustive Checks (No 'Else' needed): Compiler knows as subclasses, hence in conditional checks, like when no else needed
-  - State Management: All subclasses has it's own unique states (Properties). 
+  - State Management: All subclasses have their own unique states (Properties). 
+
 
 53. What is delegation in Kotlin?
+  - Delegation is a design pattern that transfers ownership of an object to another object
+  - A class cannot delegate the same interface to multiple objects because the compiler won't know which one to use.
+
 
 54. What is a data class in Kotlin?
+  - It is a special class that holds the state of the class, 
+  - compiler auto-generates the following methods: copy(), componentN(),
+  - Primary constructor must have min 1 parameter declared by var or val
+  - A data class cannot be abstract, open, or sealed
+
 
 55. How do you define a data class in Kotlin?
-
+  - data class Solution {}
+  
 56. What is object declaration in Kotlin?
+  - like static class (object Solution)
 
 57. How do you create a singleton in Kotlin?
+  - By object declaration
+  - Traditional Java way
+  - Companion object
 
 58. What is a companion object in Kotlin?
 
+
 59. How do you declare a companion object in Kotlin?
-
+  - inside the class 
+  
 60. What is the purpose of a companion object?
-
+    - Properties and methods that belong to the class instead of the class instance
+  
 61. What is exception handling in Kotlin?
+    - Abnormal flow of execution that is handled by a try{} catch{} block
+
 
 62. How do you throw an exception in Kotlin?
 
 63. What is a `try-catch` block in Kotlin?
 
 64. What is a checked exception in Kotlin?
+  - Kotlin does not have checked exceptions
 
 65. What are generics in Kotlin?
+  - It allow to write code that allows different data types, represented as <T>
 
+  
 66. How do you declare a generic class in Kotlin?
+  - class Box<T>(val item: T)
+  - fun <T> printItem(item: T) { ... }
 
+  
 67. What is covariance and contravariance in Kotlin?
+  - Covariance (out keyword): Covariance means you can use a Subtype where a Supertype is expected. It makes class producer
+  - Contravariance (in keyword): It means you can use a Supertype where a Subtype is expected.It makes the class a Consumer. It can only "take" (receive) the type T as an argument, but it cannot return it.
+  // Covariant: Can only return T
+  interface Producer<out T> {
+      fun produce(): T
+  }
+  
+  // Contravariant: Can only take T as input
+  interface Consumer<in T> {
+      fun consume(item: T)
+  }
 
 68. What are extension functions in Kotlin?
+  - Extension functions allow you to add new functions to an existing class without modifying its source code or inheriting from it.
+  - Such a function is written outside the class, but has a part of the class
+  - It extends the behaviour of the class, 
+  - It can be added as a plugin to the class without creating an extra instance of the class, and it does not affect existing functionality
+
 
 69. How do you declare an extension function in Kotlin?
-
+  fun String.removeFirstChar(): String {
+      return this.substring(1)
+  }
+    
 70. What is an inline class in Kotlin?
 -> It is a special type of class that wraps a single value in the class 
 -> It has only a single value in its constructor
 -> At compile time single value gets inline hence at run-time object is not created and memory saves.
+@JvmInline
+value class Solution(val value: Int)
 
 
 71. What is type aliasing in Kotlin?
+  - Providing alternative name to existing data type (typealias)
+  - typealias OnError = (message: String, code: Int) -> Unit
+  - fun fetchData(errorHandler: OnError) { ... }
+
 
 72. What is a backing field in Kotlin?
-
+  - getter and setter 
+  
 73. What is `lateinit` in Kotlin?
-
+  - late initialization
+  
 74. What is the difference between `val` and `var` in Kotlin?
+  - val : immutable property
+  - var : mutable property
 
+  
 75. What is `by lazy` in Kotlin?
+  - It is a delegate property, its preinitilized but loaded in memoryy when first time calls
 
 76. How do you declare a `by lazy` property in Kotlin?
 
@@ -337,36 +395,80 @@
 78. What is the `is` keyword in Kotlin?
 
 79. What is smart casting in Kotlin?
+  - Kotlin compiler automatically handles type casting for you.
+  - use of `is` keyword
 
 80. What is a lambda expression in Kotlin?
+  - It is annonymous function which has no name but returns results
+  - { a:Int, b:Int -> a-b }
 
+  
 81. What is the `it` keyword in Kotlin?
+  - a single parameter inside a lambda expression
+
 
 82. What are higher-order functions in Kotlin?
+  - a function which take other function as a parameter called HOF
+  - fun onSubmit(onClick:() -> Unit) {
+      onClick ()
+    }
 
+  
 83. How do you define a higher-order function in Kotlin?
-
+  - fun onSubmit(onClick:() -> Unit) {
+      onClick ()
+    }
+  
 84. What is a closure in Kotlin?
+  - A lambda or an anonymous function can access and modify variables that are defined outside its own scope.
+fun main() {
+    var counter = 0 // Variable in the outer scope
+    val increment: () -> Unit = {
+        counter++ // The lambda accesses and modifies 'counter'
+        println(counter)
+    }
+
+    increment() // Output: 1
+    increment() // Output: 2
+    increment() // Output: 3
+}
+
+
 
 85. What are inline functions in Kotlin?
+  - Compiler copy block of code of the inline function to the caller's place instead of jumping to the function's place
+
 
 86. What is the `NullPointerException` in Kotlin?
+  
 
 87. What are nullable types in Kotlin?
+  - `?` makes property nullable
 
+  
 88. How do you declare a nullable type in Kotlin?
+  - var name: String? = null
 
 89. What is the `!!` operator in Kotlin?
+  - non-null assertion operator
 
 90. What is the `?.` (safe call) operator in Kotlin?
 
 91. What is the Elvis operator (`?:`) in Kotlin?
+  - It checks for a nullable field; if null, then return the default one
+  - name ?: "Dummy Name"
 
 92. What are destructuring declarations in Kotlin?
+  - Destructuring allows you to "break apart" an object into multiple variables in a single line
+  - class Person(name: String, age:Int)
+  - val (name, age) = Person("AAA","21")
 
+  
 93. What is the purpose of the `copy()` function in a Kotlin data class?
-
+  - To update only a specific field/property of the data class
+  
 94. What is tail recursion in Kotlin?
+  - to avoid Stackoverflow, tailrec is used
 
 95. What is Kotlin's `@JvmStatic` annotation?
 
@@ -379,6 +481,7 @@
 99. What is reflection in Kotlin?
 
 100. How do you access class metadata in Kotlin?
+
 
 
 
