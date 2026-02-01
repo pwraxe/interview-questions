b2865ef4ee8bb0295da2b4572dbd85672cdd57ab
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2026-02-01 11:10:23 +0530

    Add interview questions for Harbinger interview
    Added interview questions for Prashant from Harbinger.

---
+++ Companies/Harbinger_R1_30_Jan_2026_Someones_interview_Q.kt
@@ -0,0 +1,42 @@
+Interview Quesrtion asked Prashant on 30 Jan 2026 | Harbinger
+
+
+tell me something about you
+how many YOE in android java
+tell me 1 challenging app you worked
+which application you have done in java
+Which tech-architecture you have used in android kotlin,java (like MVVM, MVC,MVP)
+
+Java
+===
+what are building blocks in java (They are expecting OOPS Pillers), Brif all those
+What are the access modifiers in java
+explain public, private, protected
+what is multiple inheritance
+why multiple inheritance in java but not in kotlin
+what is method overloading and overriding
+what is final keyword in java
+what are benefits of kotlin over java
+what is object in kotlin
+what is difference between var and val
+what is coroutine
+what is differece between launch and async
+
+Android
+=======
+What are main components in android
+what is difference between activity and fragment
+What is service
+what are types of service
+what are main lifecycle method of activity
+Which is the Running app activity state
+What is ViewModel
+What is Context
+
+Problem: same as me
+
+
+have you worked in a team?
+do you know about git and git repository
+have you uploaded apps on playstore
+have you worked on ReactNative?
