00ea3f0c3ca0a7466acc37177a322cf5bcac261c
Akshay S. Pawar <31787030+pwraxe@users.noreply.github.com>
2026-02-15 13:12:53 +0530

    Add interview questions for Accenture Pune role

---
+++ Companies/Accenture_Pune_R1_13_Feb_2026.kt
@@ -0,0 +1,34 @@
+
+- Tell me something about yourself
+- Asked about the first project in the current  organisation
+- How did you calculate a percentage in a resume, like cost-cutting in ui, api, qa?
+- You mentioned freelancing, is it full-time or part-time? Why did you choose freelancing?
+- Are you looking for full-time or part-time now? And why?
+- You mentioned you have worked 35+ apps, are all on playstore?
+- Which is the most complex project you have worked on? (Follow-up questions are created based on the answer)
+	- How did you chatting maked worked in FCM? How did you handle offline storage?
+	- If I am sending continuous messages, then how did you handle missing notifications or messages?
+	- Suppose now you have to create a chatting application, how would you create it, and what protocols would you consider?
+	- How will you add/Configure FCM in the project?  
+	- How did you make sure the message/notification is sent to the respective user only?
+	- Can we send a notification to a group of users? (Question Passed/Skipped)
+- How many YOE in Kotlin, Jetpack Compose,
+- What is the difference between Jetpack Compose and XML Layout?
+- What is recomposition in Jetpack Compose?
+- How jetpack compose saves states?
+- Have you used constraintLayout?, if Yes then how will you hide multiple views in a single line or tag?
+- Have you used Retrofit? How will you pass extra headers in Retrofit Api calls, only perticular api?
+- What is MVVM, and explain it?
+- How will you show a different layout in a single list? What will you use, and which method will you call?
+- What is differece between RecyclerView and ListView?
+- What is a sealed class in kotlin?
+- Have you worked in Agile methodology? (Scrum call), How did you handle/complete the task when you got assigned?
+- How did you resolved clashesh in existing functionality and new functionality?
+- How did you manage your Jr.s in the project?
+- Suppose you have 2 Jrs under you, 1 is working well, and the second is not doing great, how did you make the task completed?
+
+
+Ex. Create a class and receive any 2 parameters in its primary constructor, and createa  secondary constructor with extra (more than 2) parameters and callthe  first primary constructor
+create object of the class with a parameter
+
+
